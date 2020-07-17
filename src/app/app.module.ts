4eb59612b4130fdc4e8d394a4c739e5060d34d53
Chit0mx <andreschit00@gmail.com>
2020-07-17 10:29:00 -0500

    Firebase modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,44 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+// firebase libs
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore'; // firestore
+import { AngularFireStorageModule } from '@angular/fire/storage'; // storage
+import { AngularFireAuthModule } from '@angular/fire/auth'; // auth
+
+//firebase credentials
+const firebaseConfig = {
+  apiKey: 'AIzaSyD13uHBnqcFwbpC-SD6CO_yNruqTI0nXzg',
+  authDomain: 'servicios-4df28.firebaseapp.com',
+  databaseURL: 'https://servicios-4df28.firebaseio.com',
+  projectId: 'servicios-4df28',
+  storageBucket: 'servicios-4df28.appspot.com',
+  messagingSenderId: '305426721063',
+  appId: '1:305426721063:web:336620744c6056f03b6102',
+  measurementId: 'G-SB1YWT9R90',
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    //Angular Modules
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+
+    //Initialize Firebase modules
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+
+    //PWA Modules
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
