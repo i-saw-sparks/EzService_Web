4c390027ac7a633a101ba2c3ecaa8c1b3ad38955
i-saw-sparks <andresmolinaceti@gmail.com>
2020-08-16 21:48:00 -0500

    short cut cards animation details, mobile animations

--- src/app/components/short-cut-card/short-cut-card.component.ts
+++ src/app/components/short-cut-card/short-cut-card.component.ts
@@ -11,16 +11,16 @@ import { ChangeDetectorRef } from '@angular/core';
   animations: [
     trigger('fadeInState',
       [
-        state('loading', style({ opacity: 0 })),
-        state('loaded', style({ opacity: 1 })),
-        transition('loading => loaded', group([query('@fadeInIcon', animateChild()), animate('500ms')]))
+        state('loading', style({ opacity: 0, transform: 'scale(0.9)', backgroundColor: '#296096'})),
+        state('loaded', style({ opacity: 1, transform: '*',  backgroundColor: '*' })),
+        transition('loading => loaded', group([query('@fadeInIcon', animateChild()), animate('300ms')]))
       ]
     ),
     trigger('fadeInIcon',
     [
-      state('loading', style({ opacity: 0.05 })),
+      state('loading', style({ opacity: 0 })),
       state('loaded', style({ opacity: 1 })),
-      transition('loading => loaded', group([animate('500ms 500ms')]))
+      transition('loading => loaded', group([animate('400ms 300ms')]))
     ]
     )
   ], 
@@ -39,12 +39,12 @@ export class ShortCutCardComponent implements AfterViewChecked{
   }
 
   shortcuts = [
-    new cardContent('Estadisticas', ['Reportes', '?'], 'analytics', '', this.router),
     new cardContent('Usuarios', [], 'accessibility', 'root/usersCrud', this.router),
     new cardContent('Trabajadores', [], 'assignment_ind', '', this.router),
-    new cardContent('Solicitudes de registro', ['Trabajadores', 'Intermediarios'], 'all_inbox', '', this.router),
     new cardContent('Categorias', [], 'bookmarks', '', this.router),
+    new cardContent('Estadisticas', ['Reportes', '?'], 'analytics', '', this.router),
     new cardContent('Profesiones', [], 'work', '', this.router),
+    new cardContent('Solicitudes de registro', ['Trabajadores', 'Intermediarios'], 'all_inbox', '', this.router),
     new cardContent('Administradores', [], 'admin_panel_settings', '', this.router)
   ];
 
