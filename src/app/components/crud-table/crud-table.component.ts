6e827d40aec011bbc420bf14dbc8c545b6e96447
i-saw-sparks <andresmolinaceti@gmail.com>
2020-08-19 15:27:39 -0500

    Users table

--- src/app/components/crud-table/crud-table.component.ts
+++ src/app/components/crud-table/crud-table.component.ts
@@ -1,31 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-crud-table',
@@ -35,11 +13,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class CrudTableComponent {
 
   itemRef: AngularFireList<any>;
-  dataSource = ELEMENT_DATA;
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource: Observable<any>;
+  
 
   constructor(db: AngularFireDatabase) { 
     this.itemRef = db.list('Usuarios');
+    this.dataSource = this.itemRef.valueChanges();
   }
+
 }
 
