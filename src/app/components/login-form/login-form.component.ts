4ebdf63591808b82bc0e3640436f6171cc4c3f5f
i-saw-sparks <andresmolinaceti@gmail.com>
2020-09-03 15:58:21 -0500

    error message animation

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
@@ -13,29 +13,43 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         state('loaded', style({ opacity: 1 })),
         transition('loading => loaded', animate('500ms ease-out'))
       ]
+    ),
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0, backgroundColor:'#9F3131', color:'#969696' }),
+            animate('500ms ease-out',
+            style({ height: '*', opacity: 1, backgroundColor:'*', color:'*' }))
+          ]
+        )
+      ]
     )
   ]
 })
 export class LoginFormComponent implements AfterViewChecked {
   loginParams: any = {};
   loading: boolean;
-  loginFailed: boolean = false;
-  errorMessage:string;
+  loginFailed: boolean;
+  errorMessage: string;
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private cdRef: ChangeDetectorRef) {
     this.loading = true;
   }
 
   ngAfterViewChecked(): void {
     this.loading = false;
-  } 
+    this.cdRef.detectChanges();
+  }
 
   login() {
     this.authService.login(this.loginParams.email, this.loginParams.password).catch((error) => {
       this.loginFailed = true;
       this.errorMessage = error.message;
       console.log(error);
-      switch(error.code){
+      switch (error.code) {
         case 'auth/wrong-password':
           this.errorMessage = "Wrong password, try again";
           break;
